6db1a12427babdce12a32cea04be7eb9c4def392
Deto94 <19isaias94@gmail.com>
2023-04-26 20:01:58 -0300

    Arreglando CORS

--- src/main/java/com/portfolio/Deto/DetoApplication.java
+++ src/main/java/com/portfolio/Deto/DetoApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.Deto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DetoApplication {
@@ -9,5 +12,18 @@ public class DetoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DetoApplication.class, args);
 	}
+        
+        
+        @Bean
+public WebMvcConfigurer corsConfigurer() {
+  return new WebMvcConfigurer() {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+        .allowedOrigins("https://frontenddeto.web.app", "http://localhost:4200")
+        .allowedMethods("*");
+    }
+  };
+}
 
 }
